ac798a7122be3568325a419e721b7d05ed338586
Samir Alakbarov <36413513+n-droidev@users.noreply.github.com>
2025-01-13 10:23:38 +0400

    Update StepsCircularProgressBar.kt

--- progressbar/src/main/java/n_droidev/progressbar/StepsCircularProgressBar.kt
+++ progressbar/src/main/java/n_droidev/progressbar/StepsCircularProgressBar.kt
@@ -21,6 +21,20 @@ val Float.dp get() = (this * Resources.getSystem().displayMetrics.density + 0.5f
  * You can set as many colors as you want in Kotlin side with function:
  * stepsCircularProgressBar.setData(totalSteps = 9, dynamicStepColors = intArrayOf(colorInts...)) // no need to doneSteps or undoneSteps when using function
  * You can modify progress steps corners (Square, Butt, Round). You can modify its thickness and gap between them too.
+ *
+ *
+ * Copyright 2025 Samir Alakbarov
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 class StepsCircularProgressBar @JvmOverloads constructor(
     context: Context, attributeSet: AttributeSet? = null, defStyleAttr: Int = 0
